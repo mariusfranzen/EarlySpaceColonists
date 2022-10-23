6503eb8409c5325886c03dd121596f0ee3b4acb9
Marius Franzén <marius.franzen1@gmail.com>
2022-10-23 23:15:27 +0200

    1.4 patch

---
+++ 1.4/Source/EarlySpacerColonists/EarlySpacerColonists.cs
@@ -0,0 +1,52 @@
+﻿using Verse;
+using UnityEngine;
+using RimWorld;
+
+namespace EarlySpacerColonists
+{
+    public class EarlySpacerColonists : Mod
+    {
+
+        public EarlySpacerColonists(ModContentPack content) : base(content)
+        {
+            GetSettings<EarlySpacerColonists_Settings>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Early Spacer Colonists";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Gap(12f);
+            listingStandard.CheckboxLabeled(
+                Translator.TranslateSimple("shouldHaveSpacerTechLevel_title"),
+                ref EarlySpacerColonists_Settings.ShouldHaveSpacerTechLevel,
+                Translator.TranslateSimple("shouldHaveSpacerTechLevel_desc")
+            );
+
+            listingStandard.End();
+
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override void WriteSettings()
+        {
+            if (EarlySpacerColonists_Settings.ShouldHaveSpacerTechLevel)
+            {
+                DefDatabase<FactionDef>.GetNamed("EarlySpacerColonists").techLevel = TechLevel.Spacer;
+                Log.Message("Set the default techlevel to Spacer");
+            }
+            else
+            {
+                DefDatabase<FactionDef>.GetNamed("EarlySpacerColonists").techLevel = TechLevel.Industrial;
+                Log.Message("Set the default techlevel to Industrial");
+            }
+
+            base.WriteSettings();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ## Patchnotes:
 
+### 1.1.1
+Updated to Rimworld 1.4, and removing dependecy on HugsLib
+
 ### 1.1
 Added the option to start with the tech level Spacer, as mentioned in [#1](https://github.com/mariusfranzen/EarlySpacerColonists/pull/1)
 
